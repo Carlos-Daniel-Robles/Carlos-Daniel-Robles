613366e25da23579725013dd869400c7742f3443
Carlos Robles <carlosdanielroblesontiveros@gmail.com>
2020-07-17 18:43:09 -0500

    Menu de hamburgesa

---
+++ javaScript.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.menu');
+const burgerButton = document.querySelector('#burger-menu')
+
+burgerButton.addEventListener('click', hideShow);
+
+function hideShow() {
+    if (menu.classList.contains('is-active')) {
+        menu.classList.remove('is-active');
+    }
+    else {
+        menu.classList.add('is-active');
+    }
+    
+}
